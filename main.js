df1d0aaf08cc92f891fd871fd293175fe54454f3
NM-2009 <77423870+NM-2009@users.noreply.github.com>
2022-04-27 19:56:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+    video.hide();
+}
